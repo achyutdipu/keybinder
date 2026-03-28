52f9d9646cd7a5a5723851fa2e996d07f6d93162
achyutdipu <achyutdipu@gmail.com>
2026-03-28 01:03:03 -0400

    Made the GUI into an app-type window instead of a modal and it now resizes properly.

--- src/gui-interface/gui.asm
+++ src/gui-interface/gui.asm
@@ -2,11 +2,14 @@ format PE64 NX GUI 6.0
 include "gui.inc"
 entry start
 section '.data' data readable writable
+    r dd 0, 10, 0, 34
+    orr dd 0, 10, 400, 34
     key_name db "KEY_BINDING", 0
     process_name db "PROCESS_BINDING", 0
     delete_name db "DELETE_BINDING", 0
     dll_name db  "gui.dll", 0
     mdh dq 0
+    last dq 0
     j_t:
         db 0
         db key_name_len
@@ -18,7 +21,60 @@ section '.text' code readable executable
         cmp rdx, WM_INIT
         jne .cmd
         ret
+    .sz:
+        db 66h, 0fh, 6fh, 00000101b
+        dd orr - $ - 4 ; RIP-relative offset: destination - current address - offset to start of instruction
+        ; movdqa xmm0, [orr]
+        db 66h, 0fh, 7fh, 00000101b
+        dd r - $ - 4
+        ; movdqa [r], xmm0
+        test r8, r8
+        jz .fb
+        mov [last], r8
+        cmp r8, 1
+        je .f
+    .szc:
+        and r9, 0xffff
+        push r9
+        push rcx
+        mov rdx, IDC_TEXT
+        sub rsp, 20h
+        call [GetDlgItem]
+        add rsp, 20h
+        pop rcx
+        push rax
+        push rcx
+        mov rdx, r
+        sub rsp, 20h
+        call [MapDialogRect]
+        add rsp, 20h
+        pop rcx
+        mov rdx, r
+        mov r9, 1
+        sub rsp, 20h
+        call [InvalidateRect]
+        add rsp, 20h
+        pop rcx
+        pop r9
+        xor rdx, rdx
+        movsxd r8, [r+4]
+        movsxd r10, [r+12]
+        sub r10, r8
+        sub rsp, 38h
+        mov [rsp+20h], r10
+        mov qword [rsp+28h], 1
+        call [MoveWindow]
+        add rsp, 38h
+        ret
+    .fb:
+        cmp [last], 2
+        mov [last], r8
+        jne .f
+        jmp .szc
     .ncmd:
+        cmp rdx, WM_SIZE
+        je .sz 
+    .f:
         xor rax, rax
         ret
     .cmd:
@@ -44,9 +100,8 @@ section '.text' code readable executable
         sub rax, 1000
         test rax, rax
         js .endfunc
-        mov rdx, key_name
-        movzx rax, byte [j_t+rax]
-        add rdx, rax
+        movzx rdx, byte [j_t+rax]
+        add rdx, key_name
         jmp rungui
     .endfunc:
         ret
@@ -89,6 +144,10 @@ section '.idata' import readable writable
         GetForegroundWindow dq rva _GetForegroundWindow_Name
         DialogBoxParamA dq rva _DialogBoxParamA_Name
         EndDialog dq rva _EndDialog_Name
+        GetDlgItem dq rva _GetDlgItem_Name
+        MoveWindow dq rva _MoveWindow_Name
+        MapDialogRect dq rva _MapDialogRect_Name
+        InvalidateRect dq rva _InvalidateRect_Name
         dq 0
     name_table:
         _ExitProcess_Name dw 0
@@ -107,3 +166,11 @@ section '.idata' import readable writable
                            db "LoadLibraryA", 0
         _FreeLibrary_Name dw 0
                           db "FreeLibrary", 0
+        _GetDlgItem_Name dw 0
+                         db "GetDlgItem", 0
+        _MoveWindow_Name dw 0
+                         db "MoveWindow", 0
+        _MapDialogRect_Name dw 0
+                            db "MapDialogRect", 0
+        _InvalidateRect_Name dw 0
+                             db "InvalidateRect", 0
